07ee93aed94b58864fb56c4678470ac897a4ade8
ubermen5che <ubermen5ch1308@gmail.com>
2021-07-12 19:21:38 +0900

    ++) paging기능 제공을 위한 service 로직 추가

--- src/main/java/com/example/coinwiki/service/BoardService.java
+++ src/main/java/com/example/coinwiki/service/BoardService.java
@@ -1,6 +1,8 @@
 package com.example.coinwiki.service;
 
+import com.example.coinwiki.domain.PagingInfo;
 import com.example.coinwiki.domain.Post;
+import com.example.coinwiki.domain.RecivedData;
 import com.example.coinwiki.repository.BoardRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,5 +29,41 @@ public class BoardService {
 
     public List<Post> findAll(){
         return boardRepository.findAll();
+    }
+
+    public PagingInfo setPagingInfo(RecivedData rd){
+
+        int ipp = 10;
+        int page = rd.getPage();
+        int totalCount = this.findAll().size();
+        int endPage = (int)(Math.ceil(page / (double) ipp) * ipp);
+        int startPage = (endPage - ipp) + 1;
+        int totalPage = (int)(Math.ceil(totalCount / (double)ipp));
+        int tmpEndPage = (int)(Math.ceil(totalCount / (double) ipp));
+
+        if(endPage > tmpEndPage)
+            endPage = tmpEndPage;
+
+        PagingInfo pi = new PagingInfo();
+
+        pi.setTotalCount(totalCount);
+        pi.setTotalPage(totalPage);
+        pi.setStartPage(startPage);
+        pi.setEndPage(endPage);
+        pi.setPage(page);
+        pi.setIpp(ipp);
+        System.out.println(pi.toString());
+        pi.setPl(boardRepository.paging(pi));
+
+        return pi;
+    }
+
+    public List<Post> paging(){
+
+        PagingInfo pi = new PagingInfo();
+
+        pi.setIpp(10);
+
+        return boardRepository.paging(pi);
     }
 }
