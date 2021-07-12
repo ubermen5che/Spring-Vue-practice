37957fc0b114cd1df2eb5fc19546bfc09c9883b1
ubermen5che <ubermen5ch1308@gmail.com>
2021-07-12 19:16:40 +0900

    ++) 게시글 등록 라우터 링크 추가

--- front/Coinwiki/src/components/common/Header.vue
+++ front/Coinwiki/src/components/common/Header.vue
@@ -4,7 +4,7 @@
     <div class="menuWrap">
       <ul class="menu">
         <li><router-link to="/board/list">게시판</router-link></li>
-        <li><a href="javascript:;">메뉴2</a></li>
+        <li><router-link to="/board/write">게시글 등록</router-link></li>
         <li><a href="javascript:;">메뉴3</a></li>
         <li><a href="javascript:;">메뉴4</a></li>
       </ul>
