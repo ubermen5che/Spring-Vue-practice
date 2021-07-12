d9c133006adf239ef75d54fad0add7309bd0f8cf
ubermen5che <ubermen5ch1308@gmail.com>
2021-07-12 19:19:31 +0900

    **) Post DTO 수정

--- src/main/java/com/example/coinwiki/domain/Post.java
+++ src/main/java/com/example/coinwiki/domain/Post.java
@@ -1,9 +1,8 @@
 package com.example.coinwiki.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Date;
 
 @Entity
@@ -13,6 +12,75 @@ public class Post {
     private Long id;
     private String subject;
     private String cont;
+    private String type;
+    private String filename;
+    private String ori_filename;
+    private int filesize;
+    private Timestamp regDate;
+    private Timestamp editDate;
+
+    public Timestamp getEditDate() {
+        return editDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", subject='" + subject + '\'' +
+                ", cont='" + cont + '\'' +
+                ", type='" + type + '\'' +
+                ", filename='" + filename + '\'' +
+                ", ori_filename='" + ori_filename + '\'' +
+                ", filesize=" + filesize +
+                ", regDate=" + regDate +
+                ", editDate=" + editDate +
+                '}';
+    }
+
+    public void setEditDate(Timestamp editDate) {
+        this.editDate = editDate;
+    }
+
+    public Timestamp getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Timestamp regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getOri_filename() {
+        return ori_filename;
+    }
+
+    public void setOri_filename(String ori_filename) {
+        this.ori_filename = ori_filename;
+    }
+
+    public int getFilesize() {
+        return filesize;
+    }
+
+    public void setFilesize(int filesize) {
+        this.filesize = filesize;
+    }
 
     public Long getId() {
         return id;
