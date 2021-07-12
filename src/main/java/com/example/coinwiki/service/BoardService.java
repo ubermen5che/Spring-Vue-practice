d2b88cbc074ee1926a6ab74e63157b7a50c8c1b1
ubermen5che <ubermen5ch1308@gmail.com>
2021-07-13 02:52:05 +0900

    수정) 클래스명 오타 수정 및 파라미터 수정

--- src/main/java/com/example/coinwiki/service/BoardService.java
+++ src/main/java/com/example/coinwiki/service/BoardService.java
@@ -2,7 +2,7 @@ package com.example.coinwiki.service;
 
 import com.example.coinwiki.domain.PagingInfo;
 import com.example.coinwiki.domain.Post;
-import com.example.coinwiki.domain.RecivedData;
+import com.example.coinwiki.domain.ReceivedData;
 import com.example.coinwiki.repository.BoardRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,15 +23,15 @@ public class BoardService {
         return post.getId();
     }
 
-    public Optional<Post> findOne(Long id){
-        return boardRepository.findByNum(id);
+    public Post findOne(Long id, String boardCode){
+        return boardRepository.findByNum(id, boardCode);
     }
 
     public List<Post> findAll(){
         return boardRepository.findAll();
     }
 
-    public PagingInfo setPagingInfo(RecivedData rd){
+    public PagingInfo setPagingInfo(ReceivedData rd){
 
         int ipp = 10;
         int page = rd.getPage();
@@ -40,6 +40,7 @@ public class BoardService {
         int startPage = (endPage - ipp) + 1;
         int totalPage = (int)(Math.ceil(totalCount / (double)ipp));
         int tmpEndPage = (int)(Math.ceil(totalCount / (double) ipp));
+        String board_code = rd.getBoardCode();
 
         if(endPage > tmpEndPage)
             endPage = tmpEndPage;
@@ -50,10 +51,11 @@ public class BoardService {
         pi.setTotalPage(totalPage);
         pi.setStartPage(startPage);
         pi.setEndPage(endPage);
+        pi.setBoardCode(board_code);
         pi.setPage(page);
         pi.setIpp(ipp);
-        System.out.println(pi.toString());
         pi.setPl(boardRepository.paging(pi));
+        System.out.println(pi.toString());
 
         return pi;
     }
@@ -61,9 +63,9 @@ public class BoardService {
     public List<Post> paging(){
 
         PagingInfo pi = new PagingInfo();
-
+        pi.setPage(1);
         pi.setIpp(10);
-
+        pi.setBoardCode("news");
         return boardRepository.paging(pi);
     }
 }
