26503275f81de4d48dd4f6c24aeb94344d51205d
ubermen5che <ubermen5ch1308@gmail.com>
2021-07-12 19:12:17 +0900

    ++) 게시글 등록 및 조회를 위한 REST API 만듬

--- src/main/java/com/example/coinwiki/controller/BoardController.java
+++ src/main/java/com/example/coinwiki/controller/BoardController.java
@@ -1,18 +1,24 @@
 package com.example.coinwiki.controller;
 
 import com.example.coinwiki.domain.Member;
+import com.example.coinwiki.domain.PagingInfo;;
 import com.example.coinwiki.domain.Post;
+import com.example.coinwiki.domain.RecivedData;
 import com.example.coinwiki.repository.BoardRepository;
 import com.example.coinwiki.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
-@Controller
+@RestController
 public class BoardController {
 
     private BoardService boardService;
@@ -22,12 +28,38 @@ public class BoardController {
         this.boardService = boardService;
     }
 
-    @GetMapping("/api/board")
-    @ResponseBody
-    public List<Post> getBoardList(Model model){
-        List<Post> board = boardService.findAll();
-        model.addAttribute("boardList", board);
 
-        return board;
+    @GetMapping("/api/board")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    public ResponseEntity getBoardList(@RequestParam(name = "page") int pNum, RecivedData rd){
+
+        System.out.println(rd.getPage());
+
+        PagingInfo pi = boardService.setPagingInfo(rd);
+
+        HashMap<String, Object> body = new HashMap<>();
+
+        body.put("success", true);
+        body.put("pagingInfo", pi);
+
+        return new ResponseEntity(body, HttpStatus.OK);
     }
+
+    @PostMapping("/api/board")
+    public ResponseEntity create(@RequestBody PostForm form) {
+        HashMap<String, Boolean> body = new HashMap<String, Boolean>();
+
+        Post post = new Post();
+        post.setSubject(form.getSubject());
+        post.setCont(form.getCont());
+        post.setType(form.getType());
+        post.setRegDate(Timestamp.valueOf(LocalDateTime.now()));
+        body.put("success", true);
+        System.out.println(post.toString());
+
+        boardService.savePost(post);
+
+        return new ResponseEntity(body, HttpStatus.OK);
+    }
+
 }
